f01aca6cb8db6a03866b820d8525bf463e647495
dashingshakeel <shkeelrauf@gmail.com>
2017-04-12 10:26:39 +0500

    Multiple chatroom added and messaging throough channels

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  
+has_many :messages, dependent: :destroy
+def name
+  email.split('@')[0]
+end
+has_many :chat_rooms, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
